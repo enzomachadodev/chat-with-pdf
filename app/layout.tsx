3fce93e79145c60e71a90b8e2d8696003d5ecc34
Enzo Machado <enzo653fm@gmail.com>
2024-07-31 18:17:36 -0300

    feat: create landing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
-import "./globals.css";
 import { cn } from "@/utils/cn";
+import "./globals.css";
 
 const font = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
