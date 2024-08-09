affa4083875dc27bcf3c5634fa9faa86dfc595d3
Enzo Machado <enzo653fm@gmail.com>
2024-08-09 19:13:49 -0300

    build: update enviroment variables

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -2,7 +2,11 @@ import { initializeApp, getApps, App, getApp, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import { getStorage } from "firebase-admin/storage";
 
-const serviceKey = require("@/service_key.json");
+if (!process.env.SERVICE_KEY) {
+	throw new Error("SERVICE_KEY is not set");
+}
+
+const serviceKey = JSON.parse(process.env.SERVICE_KEY);
 
 let app: App;
 
@@ -18,3 +22,4 @@ const adminDb = getFirestore(app);
 const adminStorage = getStorage(app);
 
 export { app as adminApp, adminDb, adminStorage };
+
